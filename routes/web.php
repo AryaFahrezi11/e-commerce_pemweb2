c384244ca1e970dac0e8ef71fbc9f4aa865d66c5
AryaFahrezi11 <aryamaroely86@gmail.com>
2025-07-12 15:17:31 +0700

    tombol on/off

--- routes/web.php
+++ routes/web.php
@@ -65,6 +65,8 @@ Route::group(['prefix'=>'dashboard','middleware'=>['auth','verified']], function
     Route::resource('categories', ProductCategoryController::class);
     Route::resource('products', ProductController::class);
     Route::resource('themes', ThemeController::class);
+    Route::post('products/sync/{id}', [ProductController::class, 'sync'])->name('products.sync');
+    Route::post('category/sync/{id}', [ProductCategoryController::class, 'sync'])->name('category.sync');
 });
 
 // ---------------- PENGATURAN USER (Volt) ----------------
